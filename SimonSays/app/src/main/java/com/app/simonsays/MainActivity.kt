4aa698e6082811f8012d117c955083581012887e
Pol <Tihersarc35@gmail.com>
2024-02-28 17:28:33 +0100

    Activity done

--- SimonSays/app/src/main/java/com/app/simonsays/MainActivity.kt
+++ SimonSays/app/src/main/java/com/app/simonsays/MainActivity.kt
@@ -2,10 +2,18 @@ package com.app.simonsays
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val red : Button = findViewById(R.id.redButton)
+        val green : Button = findViewById(R.id.greenButton)
+        val blue : Button = findViewById(R.id.blueButton)
+        val yellow : Button = findViewById(R.id.yellowButton)
+
+        
     }
 }
